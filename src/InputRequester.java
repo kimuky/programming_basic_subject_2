7f6f1a3a8ab76078a3b3de637380de62f8dcbee2
kimuky <xogus559@gmail.com>
2024-10-22 00:08:06 +0900

    2024.10.22) fix: 기존 sc.next -> nextLine
    - InputRequester.java - next로 받을 시, 공백 단위로 끊어서 전부 입력되어, error 메시지 여러번 출력하는 버그 => nextLine으로 엔터(\n) 전까지 입력을 하나로 식별하도록 수정

--- src/InputRequester.java
+++ src/InputRequester.java
@@ -7,16 +7,16 @@ public class InputRequester {
     public String selectGameOption() {
         System.out.println("환영합니다~ 원하시는 번호를 입력해주세요.");
         System.out.println("0. 자리수 설정 1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기");
-        return sc.next();
+        return sc.nextLine();
     }
 
     public String inputNumber() {
         System.out.println("숫자를 입력해주세요:");
-        return sc.next();
+        return sc.nextLine();
     }
 
     public String selectDigit() {
         System.out.println("설정하고자 하는 자리수를 입력하세요~");
-        return sc.next();
+        return sc.nextLine();
     }
 }
